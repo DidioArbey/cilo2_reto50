89894b260aa9086818cf461777cd61a6cd75952d
Didio Perdomo <didioarbey@gmail.com>
2021-07-28 21:28:23 -0500

    metodos clase precio total

--- retogrupo50/src/PrecioTotal.java
+++ retogrupo50/src/PrecioTotal.java
@@ -9,5 +9,9 @@ public class PrecioTotal {
         this.listaEnseres = listaEnseres;
     }
     //metodos
-    
+    public void mostrarTotales(){
+        System.out.println("La suma del precio de los Enseres es de " + sumaEnseres);
+        System.out.println("La suma del precio de los Asientos es de " + sumaAsientos);
+        System.out.println("La suma del precio de los Pupitres es de " + sumaPupitres);
+    }
 }
