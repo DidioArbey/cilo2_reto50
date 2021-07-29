64c9e3eac310fa4743db071168276950fd1ad76c
Didio Perdomo <didioarbey@gmail.com>
2021-07-28 21:04:14 -0500

    crear atributos y constructores clase Pupitres

--- retogrupo50/src/Pupitres.java
+++ retogrupo50/src/Pupitres.java
@@ -1,3 +1,23 @@
-public class Pupitres {
+public class Pupitres extends Enseres{
+    //atributos
+    private Integer compartimiento;
+    private Boolean mesaTipoUniversitaria;
+    private final Integer COMPARTIMIENTO_BASE=2;
+    //Constructores
+    public Pupitres() {
+        super();
+        this.compartimiento=COMPARTIMIENTO_BASE;
+        this.mesaTipoUniversitaria=false;
+    }
+    public Pupitres(Double precioBase, Integer tamano){
+        super(precioBase, tamano);
+        this.compartimiento=COMPARTIMIENTO_BASE;
+        this.mesaTipoUniversitaria=false;
+    }
+    public Pupitres(Double precioBase, Integer tamano, String color, Integer compartimiento, Boolean mesaTipoUniversitaria){
+        super(precioBase, color, tamano);
+        this.compartimiento=COMPARTIMIENTO_BASE;
+        this.mesaTipoUniversitaria=false;
+    }
     
 }
